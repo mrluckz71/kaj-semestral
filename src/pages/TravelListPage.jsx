571aa12f83055471add1ec9f4d9c09f7cc59e328
panchluk <panchluk@fel.cvut.cz>
2025-04-16 23:53:24 +0200

    added add travel styles

--- src/pages/TravelListPage.jsx
+++ src/pages/TravelListPage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import TravelCard from "../components/TravelCard";
+import "../App.css";
 
 function TravelList() {
     return (
