5d21b9c86bf26e36597d6a95ca37a3a63a9782c8
panchluk <panchluk@fel.cvut.cz>
2025-05-21 19:18:39 +0200

    travel list page

--- src/pages/TravelListPage.jsx
+++ src/pages/TravelListPage.jsx
@@ -4,29 +4,50 @@ import React, { useEffect, useState } from 'react';
 import { auth, db } from '../firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import TravelCard from '../components/TravelCard';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+
+// const markerIcon = new L.Icon({
+//     iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+//     iconSize: [25, 41],
+//     iconAnchor: [12, 41],
+// });
 
 
 function TravelList() {
     const [trips, setTrips] = useState([]);
+    const [loading, setLoading] = useState(true);
+
 
     useEffect(() => {
         const loadTrips = async () => {
             const user = auth.currentUser;
-            if (!user) return;
+            if (!user) {
+                setLoading(false);
+                return;
+            }
 
-            const tripsRef = collection(db, "trips");
-            const q = query(tripsRef, where("userId", "==", user.uid));
-            const querySnapshot = await getDocs(q);
+            try {
+                const tripsRef = collection(db, "trips");
+                const q = query(tripsRef, where("userId", "==", user.uid));
+                const querySnapshot = await getDocs(q);
 
-            const loadedTrips = querySnapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }));
+                const loadedTrips = querySnapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }));
 
-            setTrips(loadedTrips);
+                setTrips(loadedTrips);
+                console.log("Trips loaded:", loadedTrips);
+            } catch (error) {
+                console.error("Error loading trips:", error);
+            } finally {
+                setLoading(false);
+            }
         };
 
-        loadTrips()
+        loadTrips();
     }, []);
 
     return (
@@ -34,12 +55,41 @@ function TravelList() {
             <Header />
                 <div className="travel_list_container">
                     <h2>Travel Destinations</h2>
-                        <ul className="travel_list">
+                    {/*<h4>Map of travel destinations</h4>*/}
+                    {/*<div className="map" style={{ height: "400px", width: "100%", marginBottom: "2rem" }}>*/}
+                    {/*    {!loading && (*/}
+                    {/*        <MapContainer center={[20, 0]} zoom={2} style={{ height: "100%", width: "100%" }}>*/}
+                    {/*            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />*/}
+                    {/*            {trips.map((trip) =>*/}
+                    {/*                trip.lat && trip.lng ? (*/}
+                    {/*                    <Marker*/}
+                    {/*                        key={trip.id}*/}
+                    {/*                        position={[trip.lat, trip.lng]}*/}
+                    {/*                        icon={markerIcon}*/}
+                    {/*                    >*/}
+                    {/*                        <Popup>*/}
+                    {/*                            <strong>{trip.title || trip.location}</strong><br />*/}
+                    {/*                            {trip.description}*/}
+                    {/*                        </Popup>*/}
+                    {/*                    </Marker>*/}
+                    {/*                ) : null*/}
+                    {/*            )}*/}
+                    {/*        </MapContainer>*/}
+                    {/*    )}*/}
+
+                    {/*</div>*/}
+
+                    <ul className="travel_list">
                             <li><TravelCard location='Paris' description='Lovely place' image='./assets/image.jpg' /></li>
                             <li><TravelCard location='Paris' description='Lovely place' image='./assets/image.jpg' /></li>
                             <li><TravelCard location='Paris' description='Lovely place' image='./assets/image.jpg' /></li>
                             <li><TravelCard location='Paris' description='Lovely place' image='./assets/image.jpg' /></li>
                             <li><TravelCard location='Paris' description='Lovely place' image='./assets/image.jpg' /></li>
+                            {/*{loading &&*/}
+                            {/*    <img>*/}
+                            {/*        /!*Loading... with animation to pass the point with animation*!/*/}
+                            {/*    </img>*/}
+                            {/*}*/}
                             {trips.map(trip => (
                                 <li>
                                     <TravelCard location={trip.location} description={trip.description} image={trip.image} uniqueId={trip.id} />
