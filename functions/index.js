4e64ce7450b8d61b939c14261b58aa927fe0bd9a
Lukáš Panchartek <lukypanchartek03@gmail.com>
2025-05-31 19:50:25 +0200

    polishing the css

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,13 @@
 const functions = require("firebase-functions");
 const fetch = require("node-fetch");
-
 exports.nominatimProxy = functions.https.onRequest(async (req, res) => {
+    // Handle preflight
+    if (req.method === "OPTIONS") {
+        res.set("Access-Control-Allow-Origin", "*");
+        res.set("Access-Control-Allow-Methods", "GET, OPTIONS");
+        res.set("Access-Control-Allow-Headers", "Content-Type");
+        return res.status(204).send('');
+    }
     const { q } = req.query;
     if (!q) return res.status(400).json({ error: "Missing query" });
 
